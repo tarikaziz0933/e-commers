7ff193506eff61e119f4aface33f92713dde41b5
tarikulaziz <tarikaziz0933@gmail.com>
2024-12-03 19:16:15 +0600

    added logo

--- src/SharedPages/Header/SearchBar.jsx
+++ src/SharedPages/Header/SearchBar.jsx
@@ -1,20 +1,21 @@
+import logo from "../../assets/logo/avq0lgq0t.webp"
 
 const SearchBar = () => {
     return (
         <div className=" bg-cyan-800 text-white">
             <div className="flex justify-between">
                 <div className="">
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <img className="w-20" src={logo} alt="" />
                 </div>
                 <div className="form-control">
-                    <input type="text" placeholder="Search" className="input text-black input-bordered w-24 md:w-auto" />
+                    <input type="text" placeholder="Search" className="input text-black mt-4 input-bordered w-24 md:w-auto" />
                 </div>
                 <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
                             <img
                                 alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                src={logo} />
                         </div>
                     </div>
                     <ul
